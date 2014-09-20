853e1ba667a5f605d4483033380e00b7a744cfa9
fluks <kalle.heikola@gmail.com>
2014-09-20 19:08:18 +0300

    Add javadoc for Alarm class

--- src/fi/fluks/Alarm.java
+++ src/fi/fluks/Alarm.java
@@ -1,22 +1,32 @@
 package fi.fluks;
 
-
 import java.io.File;
 import java.io.IOException;
+import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-
+/** Class for playing the alarm sound. When the target time is reached, the
+ * @{link #play() play} method on this class' instance, is called.
+ */
 public class Alarm extends ClipWrapper {
 
+    /**
+     * @param file The alarm sound file.
+     * @throws LineUnavailableException
+     * @throws UnsupportedAudioFileException
+     * @throws IOException 
+     */
     public Alarm(File file) throws LineUnavailableException,
         UnsupportedAudioFileException, IOException {
         super(file);
     }
 
     @Override
+    /** Play the alarm sound continuously.
+    */
     public void play() {
-        clip.loop(clip.LOOP_CONTINUOUSLY);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
         clip.start();
     }
 }
