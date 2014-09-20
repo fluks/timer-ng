2625dd2142469233ecb34b63e5ce1606f2a8763f
fluks <kalle.heikola@gmail.com>
2014-09-20 19:13:25 +0300

    Add javadoc for Beep class

--- src/fi/fluks/Beep.java
+++ src/fi/fluks/Beep.java
@@ -1,20 +1,30 @@
 package fi.fluks;
 
-
 import java.io.File;
 import java.io.IOException;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-
+/** Class for playing the beep sound. When interval is selected and a time
+ * set, the @{link #play() play} method on this class' instance, is called,
+ * whenever current time modulo interval target time is zero.
+ */
 public class Beep extends ClipWrapper {
 
+    /**
+     * @param file The beep sound file.
+     * @throws LineUnavailableException
+     * @throws UnsupportedAudioFileException
+     * @throws IOException 
+     */
     public Beep(File file) throws LineUnavailableException,
         UnsupportedAudioFileException, IOException {
         super(file);
     }
 
     @Override
+    /** Play the beep sound once and rewind to the beginning.
+    */
     public void play() {
         clip.start();
         clip.setFramePosition(0);
