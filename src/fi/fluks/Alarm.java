1c2a4deccd4b1d5d970c0e0ba35e2504886b8e84
fluks <kalle.heikola@gmail.com>
2014-09-20 18:55:40 +0300

    Name for package
    Sources were in a default package, name it.

--- src/fi/fluks/Alarm.java
+++ src/fi/fluks/Alarm.java
@@ -1,3 +1,5 @@
+package fi.fluks;
+
 
 import java.io.File;
 import java.io.IOException;
@@ -5,16 +7,16 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 
-public class Beep extends ClipWrapper {
+public class Alarm extends ClipWrapper {
 
-    public Beep(File file) throws LineUnavailableException,
+    public Alarm(File file) throws LineUnavailableException,
         UnsupportedAudioFileException, IOException {
         super(file);
     }
 
     @Override
     public void play() {
+        clip.loop(clip.LOOP_CONTINUOUSLY);
         clip.start();
-        clip.setFramePosition(0);
     }
 }
--- src/fi/fluks/ClipWrapper.java
+++ src/fi/fluks/ClipWrapper.java
@@ -1,3 +1,5 @@
+package fi.fluks;
+
 
 import java.io.File;
 import java.io.IOException;
